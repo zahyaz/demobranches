fb226081a699c87a4a59c02777481baf4216d56f
zahyaz <zahy.aziz@tripleten.com>
2025-10-01 15:35:50 -0400

    nice

---
+++ script.js
@@ -0,0 +1,9 @@
+// Boilerplate code for a JavaScript file
+
+// Function to initialize the script
+function init() {
+    console.log("Script initialized");
+}
+
+// Call the init function when the script loads
+init();
